663687bcb9edcc3fb44644e2c5797699bdb7d655
danchubb <danielchubb@gmail.com>
2025-01-05 15:57:55 +0000

    fixed password

--- app.py
+++ app.py
@@ -38,8 +38,6 @@ if not check_password():
     st.stop()  # Do not continue if check_password is not True.
 
 # Main Streamlit app starts here
-st.write("Here goes your normal Streamlit app...")
-st.button("Click me")
 
 # File to store timers
 TIMERS_FILE = "timers.json"
